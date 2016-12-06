ebba709b00feee7aebb46631dde9ceee09fd5cb1
unknown <jobizzness@github.io>
2016-12-06 00:38:27 +0000

    tabing

---
+++ resources/views/includes/post.blade.php
@@ -0,0 +1,101 @@
+              <div class="" style="padding: 8px 16px">
+                  <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+                    <div class="mdl-card mdl-cell mdl-cell--12-col">
+                      <div class="mdl-card__supporting-text flexed" style="margin: 40px 40px 0;">
+                        <a href="#" class="post-avatar">
+                          <img src="https://assets.materialup.com/uploads/65f40866-ec7f-4f83-adfa-965f19db92a7/avatar.jpg"        alt="Chris" width="55" height="55" class="avatar">
+                          <h3 class="posted__by-text">Amiga tepasaleh</h3>
+                        </a>
+                        <form action="#" class="mdl-cell mdl-cell--12-col">
+                          <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col">
+                            <textarea class="mdl-textfield__input" type="text" rows= "3" maxrows="6" id="feedpost" ></textarea>
+                            <label class="mdl-textfield__label" for="feedpost">What are you thinking...</label>
+                          </div>
+                        </form>
+                      </div>
+                      <div class="mdl-card__actions flexed" style="border: 0">
+                      <!-- Add spacer, to align navigation to the right -->
+                      <div class="mdl-layout-spacer"></div>
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Publish
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+              </div>
+
+
+              {{-- Post Card --}}
+                <div class="mdl-layout__tab-panel is-active" id="feed" style="margin:16px;">
+                  <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+                    <div class="mdl-card mdl-cell mdl-cell--12-col" style="border-bottom: 1px solid rgba(0, 0, 0, 0.12);">
+                      <div class="mdl-card__supporting-text flexed">
+                        <a href="#" class="post-avatar">
+                          <img src="https://assets.materialup.com/uploads/65f40866-ec7f-4f83-adfa-965f19db92a7/avatar.jpg" alt="Chris" width="55" height="55" class="avatar">
+                          <h3 class="posted__by-text">Amiga tepasaleh <span class="posted_at">3 min</span></h3>
+                        </a>
+                        <p class="post-text">Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. Pariatur occaecat nisi laboris tempor laboris eiusmod qui id Lorem esse commodo in. Exercitation aute dolore deserunt culpa consequat elit labore incididunt elit anim.</p>
+                      </div>
+                      <div class="mdl-card__actions" style="border: 0;">
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                          <i class="material-icons">thumb_up</i>
+                        </button>
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon">
+                          <i class="material-icons">thumb_down</i>
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+              </div>
+
+                            {{-- Post Card --}}
+                <div class="mdl-layout__tab-panel" id="latest">
+                  <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+                    <div class="mdl-card mdl-cell mdl-cell--12-col">
+                      <div class="mdl-card__supporting-text flexed">
+                        <a href="#" class="post-avatar">
+                          <img src="https://assets.materialup.com/uploads/65f40866-ec7f-4f83-adfa-965f19db92a7/avatar.jpg" alt="Chris" width="55" height="55" class="avatar">
+                          <h3 class="posted__by-text">Amiga tepasaleh <span class="posted_at">3 min</span></h3>
+                        </a>
+                        <p class="post-text">Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. Pariatur occaecat nisi laboris tempor laboris eiusmod qui id Lorem esse commodo in. Exercitation aute dolore deserunt culpa consequat elit labore incididunt elit anim.</p>
+                      </div>
+                      <div class="mdl-card__actions">
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                          <i class="material-icons">thumb_up</i>
+                        </button>
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon">
+                          <i class="material-icons">thumb_down</i>
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+              </div>
+
+                            {{-- Post Card --}}
+                <div class="mdl-layout__tab-panel" id="top">
+                  <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+                    <div class="mdl-card mdl-cell mdl-cell--12-col">
+                      <div class="mdl-card__supporting-text flexed">
+                        <a href="#" class="post-avatar">
+                          <img src="https://assets.materialup.com/uploads/65f40866-ec7f-4f83-adfa-965f19db92a7/avatar.jpg" alt="Chris" width="55" height="55" class="avatar">
+                          <h3 class="posted__by-text">Amiga tepasaleh <span class="posted_at">3 min</span></h3>
+                        </a>
+                        <p class="post-text">Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. Pariatur occaecat nisi laboris tempor laboris eiusmod qui id Lorem esse commodo in. Exercitation aute dolore deserunt culpa consequat elit labore incididunt elit anim.</p>
+                      </div>
+                      <div class="mdl-card__actions">
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+                          <i class="material-icons">thumb_up</i>
+                        </button>
+                        <!-- Colored icon button -->
+                        <button class="mdl-button mdl-js-button mdl-button--icon">
+                          <i class="material-icons">thumb_down</i>
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+              </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="description" content="A front-end template that helps you build fast, modern mobile web apps.">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-      <title>UrSay Gambia</title>
+      <title>UrSay The Gambia</title>
 
       <!-- Add to homescreen for Chrome on Android -->
       <meta name="mobile-web-app-capable" content="yes">
@@ -36,113 +36,37 @@
         <!-- Always shows a header, even in smaller screens. -->
             <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
               <header class="mdl-layout__header">
-                <div class="mdl-layout__header-row">
-                  <!-- Title -->
-                  <span class="mdl-layout-title">LOGO</span>
-                  <!-- Add spacer, to align navigation to the right -->
-                  <div class="mdl-layout-spacer"></div>
-                  <!-- Navigation. We hide it in small screens. -->
-                  <nav class="mdl-navigation mdl-layout--large-screen-only">
-                    <a class="mdl-navigation__link" href="">RECORDS</a>
-                    <a class="mdl-navigation__link" href="">FEED</a>
-                    <a class="mdl-navigation__link" href="">ARCHEIVE</a>
-                  </nav>
-                  <!-- Add spacer, to align navigation to the right -->
-                  <div class="mdl-layout-spacer"></div>
-                  <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-                  mdl-textfield--floating-label mdl-textfield--align-right">
-                  <label class="mdl-button mdl-js-button mdl-button--icon"
-                         for="fixed-header-drawer-exp">
-                    <i class="material-icons">search</i>
-                  </label>
-                  <div class="mdl-textfield__expandable-holder">
-                    <input class="mdl-textfield__input" type="text" name="sample"
-                           id="fixed-header-drawer-exp">
+                  <!-- Tabs -->
+                  <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
+                      <!-- Title -->
+                    <span class="mdl-layout-title">LOGO</span>
+                    <!-- Add spacer, to align navigation to the right -->
+                    <div class="mdl-layout-spacer"></div>
+
+                      <a class="mdl-layout__tab" href="#latest">LATEST</a>
+                      <a class="mdl-layout__tab is-active" href="#feed">FEED</a>
+                      <a class="mdl-layout__tab" href="#top">POPULAR</a>
+                        <!-- Add spacer, to align navigation to the right -->
+                      <div class="mdl-layout-spacer"></div>
+                      <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                      mdl-textfield--floating-label mdl-textfield--align-right">
+                      <label class="mdl-button mdl-js-button mdl-button--icon"
+                             for="fixed-header-drawer-exp">
+                        <i class="material-icons">search</i>
+                      </label>
+                      <div class="mdl-textfield__expandable-holder">
+                        <input class="mdl-textfield__input" type="text" name="sample"
+                               id="fixed-header-drawer-exp">
+                      </div>
+
                   </div>
-                </div>
                 </div>
               </header>
               {{-- Main Layout --}}
               <main class="mdl-layout__content">
-              {{-- Post Card --}}
-                <div class="mdl-layout__tab-panel is-active" id="overview">
-                  <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-                    <div class="mdl-card mdl-cell mdl-cell--12-col">
-                      <div class="mdl-card__supporting-text flexed">
-                        <a href="#" class="post-avatar">
-                          <img src="https://assets.materialup.com/uploads/65f40866-ec7f-4f83-adfa-965f19db92a7/avatar.jpg" alt="Chris" width="55" height="55" class="avatar">
-                          <h3 class="posted__by-text">Amiga tepasaleh</h3>
-                        </a>
-                        <p class="post-text">Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. Pariatur occaecat nisi laboris tempor laboris eiusmod qui id Lorem esse commodo in. Exercitation aute dolore deserunt culpa consequat elit labore incididunt elit anim.</p>
-                      </div>
-                      <div class="mdl-card__actions">
-                        <!-- Colored icon button -->
-                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
-                          <i class="material-icons">thumb_up</i>
-                        </button>
-                        <!-- Colored icon button -->
-                        <button class="mdl-button mdl-js-button mdl-button--icon">
-                          <i class="material-icons">thumb_down</i>
-                        </button>
-                      </div>
-                    </div>
-                  </section>
-              </div>
 
-              {{-- Footer stays here  --}}
-              <footer class="mdl-mega-footer">
-                <div class="mdl-mega-footer--middle-section">
-                  <div class="mdl-mega-footer--drop-down-section">
-                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Features</h1>
-                    <ul class="mdl-mega-footer--link-list">
-                      <li><a href="#">About</a></li>
-                      <li><a href="#">Terms</a></li>
-                      <li><a href="#">Partners</a></li>
-                      <li><a href="#">Updates</a></li>
-                    </ul>
-                  </div>
-                  <div class="mdl-mega-footer--drop-down-section">
-                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Details</h1>
-                    <ul class="mdl-mega-footer--link-list">
-                      <li><a href="#">Spec</a></li>
-                      <li><a href="#">Tools</a></li>
-                      <li><a href="#">Resources</a></li>
-                    </ul>
-                  </div>
-                  <div class="mdl-mega-footer--drop-down-section">
-                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Technology</h1>
-                    <ul class="mdl-mega-footer--link-list">
-                      <li><a href="#">How it works</a></li>
-                      <li><a href="#">Patterns</a></li>
-                      <li><a href="#">Usage</a></li>
-                      <li><a href="#">Products</a></li>
-                      <li><a href="#">Contracts</a></li>
-                    </ul>
-                  </div>
-                  <div class="mdl-mega-footer--drop-down-section">
-                    <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">FAQ</h1>
-                    <ul class="mdl-mega-footer--link-list">
-                      <li><a href="#">Questions</a></li>
-                      <li><a href="#">Answers</a></li>
-                      <li><a href="#">Contact us</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="mdl-mega-footer--bottom-section">
-                  <div class="mdl-logo">
-                    More Information
-                  </div>
-                  <ul class="mdl-mega-footer--link-list">
-                    <li><a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a></li>
-                    <li><a href="#">Help</a></li>
-                    <li><a href="#">Privacy and Terms</a></li>
-                  </ul>
-                </div>
-              </footer>
+              @include('includes/post')
+              @include('includes/footer')
             </main>
             </div>
     </body>
